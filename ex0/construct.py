87072d5dd3d4892d5e96847e0b6db40d8bff1ede
spacotto <silvia.pacotto@gmail.com>
2026-04-02 13:55:44 +0200

    Create construct.py

---
+++ ex0/construct.py
@@ -0,0 +1,7 @@
+"""
+Exercise 0: Entering the Matrix
+Authorized: sys, os, site modules, print().
+
+demonstrates your understanding
+of Python virtual environments. The machines have corrupted the global Python installation, and you need to create a clean, isolated space to work.
+"""
